47c9e34cfbe207e46caf414a88293723e3b46053
evelinj41 <39770138+evelinj41@users.noreply.github.com>
2018-07-25 20:12:17 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,69 @@
 jQuery(document).ready(function($){
-	$('.name').on(
-		'submit',
-		function(e){
+  $('.registro').on(
+    'submit',
+    function(e){
     e.preventDefault();
 
-    var nombre = $('.registro .name').val();
-  var apellidos = $('.registro .apellido').val();
 
-  if (!nombre) {
-  	alert('debe ingresar un nombre.');
-  } else if(!apellidos){
-  	alert('debe ingresar un apellido.');
+  var name = $('.registro .name').val();
+  var apellido = $('.registro .apellido').val();
+  var number = $('.registro.number').val();
+  var Email = $('.registro .Email').val();
+  var edad = $('.registro .edad').val();
+  var dep = $('.registro .dep').val();
+  var number = $('.registro .dep').val();
+  var des = $('.registro .des').val();
+  var ciu = $('.registro .ciu').val();
+
+  if (!name) {
+    alert('debe ingresar el campo.');
+  } else if(!apellido){
+    alert('debe ingresar el campo.');
   } else {
     firebase
     .auth()
-    .signInWithNameAndApellidos(nombre, apellidos)
+    .createUserWithNameAndApellido(name, apellido, number, Email, edad, dep, number)
     .then(function(){
-    	alert('bienvenido');
+      alert('bienvenido');
     })
     .catch(function(error) {
-    	alert('no eres bienvenido');
+      alert('no eres bienvenido');
     
          });
- 	  }
- 	}
+    }
+  }
+
+  );
+
+});
+
+jQuery(document).ready(function($){
+  $('.registro').on(
+    'submit',
+    function(e){
+    e.preventDefault();
+alert('ingrsar evento');
+  var number = $('.numero.number').val();
+  var Email = $('.numero .Email').val();
+  var edad = $('.numero .edad').val();
+
+  if (!number) {
+    alert('debe ingresar un numero.');
+  } else if(!Email){
+    alert('debe ingresar un email.');
+  }else {
+    firebase
+    .auth()
+    .createUserWithNumberAndEmail(number, Email, edad)
+    .then(function(){
+      alert('bienvenido');
+    })
+    .catch(function(error) {
+      alert('no eres bienvenido');
+    
+         });
+    }
+  }
 
   );
 
