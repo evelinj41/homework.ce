97efb2d8897f32a25d9d873fc2d947a4197e4061
evelinj41 <katerinmejia.13@gmail.com>
2018-08-04 08:57:33 -0500

    repo para cristian

--- firebase.js
+++ firebase.js
@@ -43,7 +43,7 @@ $('.button').on('click', function( event ){
   });
 }); 
 
-$('.button').on('click', function( event ){
+$('.buton').on('click', function( event ){
   event.preventDefault();
 
   var dato = {
