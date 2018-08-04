b708189d0491bf82172b27c75bc998c35c265551
evelinj41 <katerinmejia.13@gmail.com>
2018-08-03 20:20:15 -0500

    holaa

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Initialize Firebase
   var config = {
     apiKey: "AIzaSyClhlxMkx7r6Ph0B7o_Y0iaNvzS2Rqjt78",
@@ -43,20 +42,36 @@ $('.button').on('click', function( event ){
     };
   });
 }); 
-=======
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyClhlxMkx7r6Ph0B7o_Y0iaNvzS2Rqjt78",
-    authDomain: "homeworkce-e9008.firebaseapp.com",
-    databaseURL: "https://homeworkce-e9008.firebaseio.com",
-    projectId: "homeworkce-e9008",
-    storageBucket: "homeworkce-e9008.appspot.com",
-    messagingSenderId: "1077796373586"
-  };
-  firebase.initializeApp(config);
-  const database = firebase.database();
 
-   var name = $ ('.name').val();
-   var number = $ ('.number').val();
-   var lugares = $ ('.container').val();
->>>>>>> d71da5af8573225c7b92be57ccaa9c0f96804b9a
+$('.button').on('click', function( event ){
+  event.preventDefault();
+
+  var dato = {
+    password:$('.password').val(),
+    email:$('.email').val(),
+    name:$('.name').val(),
+    apellido:$('.apellido').val(),
+    cel:$('.cel').val(),
+    number:$('.number').val(),
+    genero:$('.genero').val(),
+    dep:$('.dep').val(),
+    doc:$('.doc').val(),
+    ciu:$('.ciu').val(),
+    des:$('.des').val(),
+    nombre:$('.nombre').val(),
+    apellid:$('.apellid').val(),
+    numero:$('.numero').val(),
+    nickname:$('.nickname').val(),
+    apelli:$('.apelli').val(),
+    numbero:$('.numbero').val(),
+    user:$('.user').val()
+  }; 
+  console.log(dato);
+  database.ref('/users/').push(dato, function(error) {
+    if (error) { throw error; }
+    else {
+      console.info ('guardamos la información');
+      location.href="Adentro.html"
+    };
+  });
+}); 
